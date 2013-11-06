bec639b6a09ff0f6fbc0befdfdce70c58ac64d8c
rajanand02 <raj_anand02@yahoo.in>
2013-11-06 18:32:53 +0530

    configure twitter account

--- client/twitter_login.js
+++ client/twitter_login.js
@@ -1,14 +1,3 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to twitter_login.";
-  };
-  Template.content.users = function () {
-    return Meteor.users.find();
-  };
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+Template.hello.users = function (){
+  return Meteor.users.find();
+};
